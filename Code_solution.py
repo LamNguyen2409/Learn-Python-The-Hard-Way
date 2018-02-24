91041d7335973f1972ba8cb1592a2073c1fee9f6
Lam Nguyen <dieulap@gmail.com>
2018-02-24 19:33:18 +0700

    Update Code_solution.py

--- Code_solution.py
+++ Code_solution.py
@@ -34,3 +34,41 @@ print 'I "said" to not touch this.'
 
 print " This will run."
 print " Today #It rains"
+
+#Exercise 3: Numbers and Math
+#Print into screen this sentence for notify news "I will now count my chickens:"
+print "I will now count my chickens:"
+
+#Print amount of Hens and Roosters 
+print "Hens",25+30/6 # print text in "" and result of operation -> Hens 30
+print "Roosters", 100-25*3%4 
+
+#Print a sentence to screen
+print "Now I will count the eggs:"
+
+# % modulo function 4%2=2, 
+print 3+2+1-5+4%2-1/4+6
+
+print "Is it true that 3+2<5-7?"
+
+print 3+2 < 5-7
+
+print "What is 3+2?", 3+2
+print "What is 5-7?",5-7
+
+#7%4=3 Modulo function
+print 7%4
+print "What is 17%4", 17%4
+print "What is 1/4?", 1/4 #if number1, number2 are interger type, result is interger
+print "What is 1.0/4?", 1.0/4 # if one of all is decimal number, result is decimal number
+
+print "Oh, that's why it's False."
+
+print "how about some more."
+
+# Compare 2 numbers, if 5>-2, get TRUE, if 5 <=-2 get FALSE
+print "Is it greater?",5>-2
+print "Is it greater or equal?", 5>=-2
+print "Is it less or equal?", 5<=-2
+
+
