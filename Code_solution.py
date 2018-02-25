ab7267c9a8d951e0966612cb39044c009e5e3699
Lam Nguyen <dieulap@gmail.com>
2018-02-25 12:28:58 +0700

    Update Code_solution.py

--- Code_solution.py
+++ Code_solution.py
@@ -107,6 +107,29 @@ print "His teeth are usually %s depending on the coffee." % my_teeth
 
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." %(my_age, my_height, my_weight, my_age + my_height +  my_weight)
+#Study drill
+#1. try to remove all text "my_" in the script
+name = 'Zed A.Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = "White"
+hair = 'Brown'
+
+print " Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." %(age, height, weight, age + height +  weight)
+#2. try to write some variables that convert the inches and pounds to centimeters and kilograms 
+# print "He's %d inches tall." % (height *2.54)
+# print "He's %f inches tall." % (height *2.54)
+#3. Search online for all of the Python format characters : 
 
 #Exercise 6: String and text
 
